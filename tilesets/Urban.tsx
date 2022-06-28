cbda0b93d57ffad2d12789befc321911b013df80
Audrey Hester <ahester23@governors.school>
2022-06-28 18:20:32 +0200

    what

---
+++ tilesets/Urban.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Urban" tilewidth="16" tileheight="16" spacing="1" tilecount="486" columns="27">
+ <image source="../../Downloads/kenney_rpgUrbanKit/Tilemap/tilemap.png" width="458" height="305"/>
+</tileset>

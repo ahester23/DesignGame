3e3d6940d6508931d40f8bb6bcf2c20ad0724e38
Audrey Hester <ahester23@governors.school>
2022-07-01 13:41:33 +0200

    hi

--- tilesets/WA_Seats.tsx
+++ tilesets/WA_Seats.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="WA_Seats" tilewidth="32" tileheight="32" tilecount="182" columns="13">
+<tileset version="1.9" tiledversion="1.9.0" name="WA_Seats" tilewidth="32" tileheight="32" spacing="1" tilecount="156" columns="12" tilerendersize="grid">
  <image source="WA_Seats.png" width="416" height="448"/>
 </tileset>

a6c2f52c646d74d7d18271caab691ed54bcfd636
Audrey Hester <ahester23@governors.school>
2022-07-05 00:49:45 +0200

    script for chat

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
+let hintLevel: 1;
+let id = 1;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
@@ -27,16 +29,48 @@ WA.onInit().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 
-    WA.chat.onChatMessage((message => {
+    WA.chat.onChatMessage(((message: string) => {
         if (id == 1){
             // do something
+            WA.chat.sendChatMessage('Hello, I am Mr.Robot.', 'Mr Robot');
+            id ++;
         } else {
             if (id == 2){
+                WA.chat.sendChatMessage('Do you need a hint?', 'Mr Robot');
+                WA.chat.onChatMessage((message: string) => {
+                    if (message == 'y'){
+                        // print hint
+                        WA.chat.sendChatMessage('Hint 1', 'Mr Robot');
+                        hintLevel ++;
+                    } 
+                    else {
+                        if (message == 'n'){
+                            id++
+                        }
+                    }
+                })
             }
-        }
+
+            if (id == 3){
+                WA.chat.sendChatMessage('Please enter the code.', 'Mr Robot');
+                WA.chat.onChatMessage((message: string) => {
+                    if (message == '1234'){
+                        // print hint
+                        WA.chat.sendChatMessage('You entered the right code!', 'Mr Robot');
+                        WA.chat.sendChatMessage('The secret password is star. Goodbye.', 'Mr Robot');
+                        id ++;
+                    } 
+                    else {
+                        if (message != '1234'){
+                            WA.chat.sendChatMessage('You entered the wrong code!', 'Mr Robot'); 
+                            id = 1; 
+                        }
+                    }
+                })
+            }
         console.log('The user typed a message', message);
         WA.chat.sendChatMessage('Hello world you wrote: ' + message, 'Mr Robot');
-    }));
+    }}));
 
 
 }).catch(e => console.error(e));

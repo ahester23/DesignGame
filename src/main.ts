78aa9c63b496fba14e88e893222001d8453774df
Audrey Hester <ahester23@governors.school>
2022-07-06 11:47:01 +0200

    chat working

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,19 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
-// var hintLevel: 1;
+
 // var id = 1
+var hint1 = 'hint1'
+var hint2 = 'hint2'
+var hint3 = 'hint3'
+var directions = 'directions'
+var introduction = 'Riddle Introduction'
+var robotIntro = 'Hello, bla bla bla. I do all these things.'
+var robotCom = 'Robot commands here.'
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
+    var hintLevel = 1;
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
@@ -24,28 +32,96 @@ WA.onInit().then(() => {
 
     WA.room.onLeaveLayer('clockZone').subscribe(closePopUp);
 
+    WA.chat.sendChatMessage(robotIntro, 'Mr. Robot')
+    console.log('User introduced to Mr. Robot.')
+    WA.chat.sendChatMessage(introduction, 'Mr. Robot')
+    console.log('Riddle introduced to user.')
+    WA.chat.sendChatMessage(directions, 'Mr.Robot')
+    console.log('Directions sent to user.')
+    WA.chat.sendChatMessage("How can I help you today?", 'Mr Robot');
+    console.log('Asked user what help they need.');
     WA.chat.onChatMessage((message => {
+        console.log('User sent :', message);
         switch (message) {
+            case 'help':
+                WA.chat.sendChatMessage('How can I help you?', 'Mr. Robot')
+                break
+            case 'instructions':
+                WA.chat.sendChatMessage(directions, 'Mr.Robot')
+                break
+            case 'reprint':
+                WA.chat.sendChatMessage(hint1, 'Mr Robot');
+                WA.chat.sendChatMessage(hint2, 'Mr Robot');
+                WA.chat.sendChatMessage(hint3, 'Mr Robot');
+                console.log('All hints reprinted.')
+                break
+
+            case "none":
+                console.log('User said they do not need help. Wait for next message.');
+                WA.chat.sendChatMessage('Okay, I am here if you need me!', 'Mr. Robot')
+                break
+            
             case "code":
-                WA.chat.sendChatMessage('Ok, you may enter your code now.', 'Mr Robot');
-                WA.chat.onChatMessage((message) => {
-                    switch(message){
-                        case "1234":
-                            WA.chat.sendChatMessage('You answered correctly. The passcode is star.', 'Mr. Robot')
-                            break
-                        default:
-                            WA.chat.sendChatMessage('You answered incorrectly. Better luck next time!')
-                    }
-                })
+                console.log('User would like to enter code.');
+                WA.chat.sendChatMessage('Ok, you may enter your code now.', 'Mr. Robot');
+                console.log('Prompted user for code.');
                 break;
+
             case "hint":
-                WA.chat.sendChatMessage('Butterbrot 1', 'Mr Robot');
+                console.log('User asked for a hint.');
+                if (hintLevel == 1){
+                    WA.chat.sendChatMessage('Here is your first hint:', 'Mr. Robot');
+                    WA.chat.sendChatMessage(hint1, 'Mr Robot');
+                    console.log('First hint given to user.');
+                }
+
+                else if (hintLevel == 2){
+                    WA.chat.sendChatMessage('Here is your second hint:', 'Mr. Robot');
+                    WA.chat.sendChatMessage(hint2, 'Mr Robot');
+                    console.log('Second hint given to user.');
+                }
+
+
+                else if (hintLevel == 3){
+                    WA.chat.sendChatMessage('Here is your third hint:', 'Mr. Robot');
+                    WA.chat.sendChatMessage(hint3, 'Mr Robot');
+                    console.log('Third hint given to user.')
+                }
+
+                else{
+                    WA.chat.sendChatMessage('You are out of hints. If you would like to see them all again, type reprint.', 'Mr. Robot');
+                    console.log('User has used all hints. Asked about re-printing them.')
+                }
+
+                hintLevel ++ 
                 break;
+ 
             default: 
-                WA.chat.sendChatMessage('Would you like a hint or to enter the code?', 'Mr Robot');
-        } 
-        }
-    ));
+            if (isNaN(parseInt(message))){
+                // rightCode = false
+                console.log('User did not enter a code')
+            }
+            else{
+                console.log('User entered a code.')
+                if (parseInt(message) == 1234){
+                    console.log('Code is correct.')
+                    console.log('Code is entered and correct.')
+                    WA.chat.sendChatMessage('You answered the correct code. The passcode is star.', 'Mr. Robot')
+                    console.log('Tell user code is correct');
+                    break
+                    }
+                else{
+                    console.log('Code is entered but incorrect.')
+                    WA.chat.sendChatMessage('You answered the incorrect code.', 'Mr. Robot')
+                    console.log('Tell user the code is incorrect.');
+                }
+            }
+            WA.chat.sendChatMessage(robotCom, 'Mr. Robot')
+            break
+            }
+
+    }))
+
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {

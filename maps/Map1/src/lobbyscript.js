3394d7efaef577321c1d825299481b1153350e1a
Audrey Hester <ahester23@governors.school>
2022-07-13 23:00:06 +0200

    Revert "nf"
    This reverts commit 01abe64711fd26f9ecf8d73abd514f7c834cefb8.

--- maps/Map1/src/lobbyscript.js
+++ maps/Map1/src/lobbyscript.js
@@ -3,7 +3,7 @@ console.log('hello world!')
 
 
 
-import { bootstrapExtra } from "../../../node_modules/@workadventure/scripting-api-extra";
+import { bootstrapExtra } from "../../../node_modules/@workadventure/scripting-api-extra/dist";
 
 console.log('Script started successfully');
 

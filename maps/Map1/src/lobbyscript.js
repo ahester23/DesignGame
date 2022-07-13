b3ba0eaf3be11a99616da73bd3f1eeaaac98ed46
Audrey Hester <ahester23@governors.school>
2022-07-13 22:59:34 +0200

    Revert "what"
    This reverts commit 98c41325ca5df2a3011df366bf66c88d539c3c71.

--- maps/Map1/src/lobbyscript.js
+++ maps/Map1/src/lobbyscript.js
@@ -3,7 +3,7 @@ console.log('hello world!')
 
 
 
-import { bootstrapExtra } from "../../../node_modules/@workadventure/scripting-api-extra/dist/bootstrap.js";
+import { bootstrapExtra } from "../../../dist/bootstrap";
 
 console.log('Script started successfully');
 

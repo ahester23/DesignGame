3b000484b11f1beadfc1be9aa2aa1a9386f2c1ec
Audrey Hester <ahester23@governors.school>
2022-07-13 16:47:38 +0200

    lol

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { getMapsOptimizers, getMapsScripts } from "wa-map-optimizer-vite";
 
 export default defineConfig({
-    base: "./",
+    base: "./dist",
     build: {
         rollupOptions: {
             input: {

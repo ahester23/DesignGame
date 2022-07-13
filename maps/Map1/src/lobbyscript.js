4a8e909f42e20568916bae0fe2a38e0583d0f01b
Audrey Hester <ahester23@governors.school>
2022-07-13 22:59:41 +0200

    Revert "nah"
    This reverts commit bedf6bdcd8e28fe53d506014be4eed63b0319cc1.

--- maps/Map1/src/lobbyscript.js
+++ maps/Map1/src/lobbyscript.js
@@ -3,7 +3,7 @@ console.log('hello world!')
 
 
 
-import { bootstrapExtra } from "../../../dist/bootstrap";
+import { bootstrapExtra } from "C:\Users\hester\Documents\GitHub\Pfaff\dist\bootstrap.js";
 
 console.log('Script started successfully');
 

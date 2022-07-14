90e68d46fc3cd7ddac394cb9c5743c0eac653b93
Audrey Hester <ahester23@governors.school>
2022-07-14 23:15:36 +0200

    try

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     server: {
         host: "localhost",
         headers: {
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Origin": "null",
             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
             "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
             "Cache-Control": "no-cache, no-store, must-revalidate",
